e3f558cc24e93539abb0a23bca19ff52c3860c96
shenbei <1183515762@qq.com>
2015-09-25 11:56:51 +0800

    创建snake（贪吃蛇）

---
+++ Game/app/src/main/java/com/shen/game/snake/GameView.java
@@ -0,0 +1,43 @@
+package com.shen.game.snake;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by shen on 15/9/25.
+ */
+public class GameView extends SurfaceView implements SurfaceHolder.Callback{
+    public GameView(Context context) {
+        super(context);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
